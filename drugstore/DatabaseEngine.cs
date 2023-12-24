7d2f04b315f51b092be874672a1d7c639019d131
WonderJane <marytun2003@gmail.com>
2023-12-24 12:24:23 +0300

    Добавьте файлы проекта.

---
+++ drugstore/DatabaseEngine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace drugstore
+{
+    class DatabaseEngine
+    {
+        public static string ConnectionString { get; set; }
+        public static string Command { get; set; }
+        public static NpgsqlConnection Connection { get; set; }
+        DatabaseEngine()
+        {
+            ConnectionString = "Host=localhost;Username=postgres;Password=1234;Database=drugs";
+            Connection = new NpgsqlConnection(ConnectionString);
+        }
+        public async void Open()
+        {
+            try
+            {
+                await Connection.OpenAsync();
+            }
+            catch(Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message, "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public async void Close()
+        {
+            await Connection.CloseAsync();
+        }
+        public async void Do(string command)
+        {
+            Command = command;
+            var nc = new NpgsqlCommand(Command, Connection);
+            try
+            {
+                await nc.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message, "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void FillTable(DataGrid grid, string command)
+        {
+            Command = command;
+            var nc = new NpgsqlCommand(Command, Connection);
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter();
+            adapter.SelectCommand = nc;
+
+            DataTable table = new DataTable();
+            table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+            adapter.Fill(table);
+            
+            grid.DataSource = table;
+        }
+    }
+}
---
+++ drugstore/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace drugstore
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Close.Margin = new Thickness(Width / 3, 0, 0, 0);
+            Start.Margin = new Thickness(Width / 3 * -1, 0, 0, 0);
+        }
+
+        private void grdHeader_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close.Foreground = new SolidColorBrush(Colors.Crimson);
+            this.Close();
+        }
+
+        private void Close_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Close.FontSize += 2;
+            Close.Foreground = new SolidColorBrush(Color.FromRgb(255, 201, 182));
+        }
+
+        private void Close_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Close.FontSize -= 2;
+            Close.Foreground = new SolidColorBrush(Colors.Coral);
+        }
+
+        private void Start_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Тут пока ничего нет");
+        }
+
+        private void Start_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Start.FontSize += 2;
+            Start.Foreground = new SolidColorBrush(Color.FromRgb(221, 255, 129));
+        }
+
+        private void Start_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Start.FontSize -= 2;
+            Start.Foreground = new SolidColorBrush(Color.FromRgb(129, 255, 186));
+        }
+    }
+}
